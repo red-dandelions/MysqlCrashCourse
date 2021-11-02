4e65963181165261436d2528a45c5d8c9832f7b1
Ice-vanilla <1011694931@qq.com>
2021-11-02 19:37:08 +0800

    add chapter5

--- cpp/chapter5/main.cpp
+++ cpp/chapter5/main.cpp
@@ -57,55 +57,49 @@ void func(Session &sess) {
 
     cout << setiosflags(ios::fixed) << setprecision(2) << setiosflags(ios::left);
 
-    //4-2
-    //select prod_name from products;
-    print("4-2");
-    cout << "sql: select prod_name from products;" << endl;
-    res = tb.select("prod_name").execute();
+    //5-1
+    //select prod_name from products order by prod_name;
+    print("5-1");
+    cout << "sql: select prod_name from products order by prod_name;" << endl;
+    res = tb.select("prod_name")
+            .orderBy("prod_name")
+            .execute();
     output(res);
     cout << endl;
 
-    //4-3
-    //select prod_id, prod_name, prod_price from products;
-    print("4-3");
-    cout << "sql: select prod_id, prod_name, prod_price from products;" << endl;
-    res = tb.select("prod_id", "prod_name", "prod_price").execute();
+    //5-2
+    //select prod_id, prod_price, prod_name from products order by prod_price, prod_name;
+    print("5-2");
+    cout << "sql: select prod_id, prod_price, prod_name from products "
+            "order by prod_price, prod_name;" << endl;
+    res = tb.select("prod_id", "prod_price", "prod_name")
+            .orderBy("prod_price", "prod_name")
+            .execute();
     output(res);
     cout << endl;
 
-    //4-4
-    //select * from products;
-    print("4-4");
-    cout << "sql: select * from products;" << endl;
-    res = sess.sql("select * from products").execute();
+    //5-3
+    //select prod_id, prod_price, prod_name
+    //from products
+    //order by prod_price DESC;
+    print("5-3");
+    cout << "sql: select prod_id, prod_price, prod_name "
+            "from products "
+            "order by prod_price DESC;" << endl;
+    res = tb.select("prod_id", "prod_price", "prod_name")
+            .orderBy("prod_price DESC")
+            //.bind("param", "DESC")
+            .execute();
     output(res);
     cout << endl;
 
-    //4-5
-    //select DISTINCT vend_id from products;
-    print("4-5");
-    cout << "sql: select distinct vend_id from products;" << endl;
-    res = sess.sql("select DISTINCT vend_id from products").execute();
+    cout << "sql: select prod_price from products order by prod_price DESC limit 1;" << endl;
+    res = tb.select("prod_price")
+            .orderBy("prod_price DESC")
+            .limit(1)
+            .execute();
     output(res);
     cout << endl;
-
-    //4-6
-    //select prod_name from products limit 5;
-    print("4-6");
-    cout << "sql: select prod_name from products limit 10;" << endl;
-    res = tb.select("prod_name").limit(10).execute();
-    output(res);
-    cout << endl;
-
-    //select prod_name from products limit 5 offset 5;
-    cout << "sql: select prod_name from products limit 5 offset 5;" << endl;
-    res = tb.select("prod_name").limit(5).offset(5).execute();
-    output(res);
-    cout << endl;
-
-    //4-7
-    //限定名
-    //select products.prod_name from test.products;
 
 }
 
@@ -160,7 +154,7 @@ void output(RowResult& rres) {
             cout << "|" << endl;
         }
     };
-
+    
     updateCol();
     outline();
     outColumnName();
